f725a0896fdff80d49067074c0e66d51fda43567
evewandeto <evalynewanjiku656>
2022-09-23 13:51:23 -0700

    o

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  *  _strcat - a function that concatenates two strings
+ *   *  @dest: copy to
+ *    *  @src: copy from
+ *     *  Return: two concat strings
+ *      */
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+	dest[dlen] = '\0';
+	return (dest);
+}
